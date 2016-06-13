6b5729764f59cce6a10676344227c608c4f8d558
Mikko Yliniemi <mikessu@iki.fi>
2016-06-13 16:09:50 +0300

    updated clang dockerfile

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -3,3 +3,5 @@ set -e -x
 docker build $* -t libfuzzer/base base/
 docker build $* -t libfuzzer/base-clang base-clang/
 docker build $* -t libfuzzer/base-fuzzer base-fuzzer/
+
+docker build $* -t libfuzzer/libxml2 libxml2/
