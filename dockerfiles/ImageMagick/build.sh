538c42b8c01d2c4400f253573d674d5353cefbf3
Atte Kettunen <attekett@gmail.com>
2016-06-14 23:09:00 +0300

    fixed ImageMagick coverage collection, third-party libs not instrumented

--- dockerfiles/ImageMagick/build.sh
+++ dockerfiles/ImageMagick/build.sh
@@ -7,24 +7,25 @@ export CC="clang"
 export LDFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
 export LIBFUZZER_OBJS="/work/libfuzzer/*.o"
 
+#Build libFuzzer
 mkdir -p /work/libfuzzer; 
 cd /work/libfuzzer; 
 for f in /src/llvm/lib/Fuzzer/*cpp; do 
 	$CXX -std=c++11 -fsanitize=address -IFuzzer -c $f 
 done 
 
+#Build ImageMagick
 cd /src/ImageMagick
-./configure --without-threads --disable-openmp --without-gslib --without-dpsv --with-modules
+./configure --prefix=/usr/ --without-magick-plus-plus --without-perl --without-threads --without-tiff --with-gslib=no --without-dps 
 make -j4
+make install
 
-#$CXX $CXXFLAGS -std=c++11 -I$(pwd) $(pwd)/MagickCore/*.o $(pwd)/coders/*.o $(pwd)/filters/*.o \
-#			-lxml2 -lm -lpthread -lz -lX11 -lfontconfig -lfreetype -llzma -fopenmp -lpng -ltiff \
-#			-lXext -lrt -ljbig -ljpeg -lcairo -lpango-1.0 -lgobject-2.0 -lbz2 -lpangocairo-1.0 \
-#			$LIBFUZZER_OBJS -o ImageMagick_fuzzer ImageMagick_fuzzer.c
 
-$CXX $CFLAGS -std=c++11 -I$(pwd) $(pwd)/MagickCore/*.o  -lxml2 -lm -lpthread -lz -lX11 -lfontconfig \
+#Build fuzzer
+$CXX $CFLAGS -std=c++11 -I$(pwd) /usr/lib/libMagick*.so -lxml2 -lm -lpthread -lz -lX11 -lfontconfig \
 			 -lfreetype -llzma -fopenmp -lpng -ltiff -lXext -lrt -ljbig -ljpeg -lcairo -lpango-1.0 \
 			 -lgobject-2.0 -lbz2 -lpangocairo-1.0 -llcms2 -llqr-1 -lfftw3 -lltdl \
 			 /work/libfuzzer/Fuzzer*.o  -o ImageMagick_fuzzer ImageMagick_fuzzer.c 
 
-
+#Ghostcript is annoying
+apt-get remove ghostscript -y
