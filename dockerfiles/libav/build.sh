461e83735b3bbb80639428a2764852d95e54910b
Mikko Yliniemi <mikessu@iki.fi>
2016-06-14 15:20:47 +0300

    added libav(not working yet)

---
+++ dockerfiles/libav/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export CXXFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+export CXX="clang++"
+export CFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+export CC="clang"
+export LDFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+
+
+cd /src/libav/
