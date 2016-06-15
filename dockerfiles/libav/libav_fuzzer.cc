a01dfd351ffa2a6f63153e40fce6ce744bb56901
Mikko Yliniemi <mikessu@iki.fi>
2016-06-15 16:01:18 +0300

    modified libav fuzzer, not ready at all

--- dockerfiles/libav/libav_fuzzer.cc
+++ dockerfiles/libav/libav_fuzzer.cc
@@ -1,11 +1,24 @@
-// Copyright (c) 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+#include <string.h>
+#include <stdint.h>
 #include "libavcodec/avcodec.h"
-void ignore(void *ctx, const char *msg, ...) {
-  // Error handler to avoid spam of error messages from libxml parser.
-}
-extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+
+#define INBUF_SIZE 4096
+#define AUDIO_INBUF_SIZE 20480
+#define AUDIO_REFILL_THRESH 4096
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+
+  c = avcodec_alloc_context3(codec);
+  c->request_sample_fmt = AV_SAMPLE_FMT_S16;
+
+  avpkt.data = inbuf;
+  avpkt.size = fread(inbuf, 1, AUDIO_INBUF_SIZE, f);
+
+
+
+     fclose(outfile);
+     fclose(f);
+     free(outbuf);
 
   return 0;
 }
