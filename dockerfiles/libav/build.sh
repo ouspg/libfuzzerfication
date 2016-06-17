0ced51bbb5d6469a97c06f01cfafba7e4abf50e7
Mikko Yliniemi <mikessu@iki.fi>
2016-06-17 11:12:59 +0300

    modified libav files

--- dockerfiles/libav/build.sh
+++ dockerfiles/libav/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-set -e
 
-export CXXFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+export CXXFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
 export CXX="clang++"
-export CFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+export CFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
 export CC="clang"
-export LDFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+export LDFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
+export LIBFUZZER_OBJS="/home/mikko/work/libfuzzer/*.o"
+
+#Build libav
+cd /home/mikko/src/libav
+./configure --prefix=$HOME
+#make -j4
+#make install
 
 
-cd /src/libav/
+#Build fuzzer
+#$CXX $CFLAGS -std=c++11 -I$(pwd) /usr/lib/libMagick*.so -lxml2 -lm -lpthread -lz -lX11 -lfontconfig \
+#			 -lfreetype -llzma -fopenmp -lpng -ltiff -lXext -lrt -ljbig -ljpeg -lcairo -lpango-1.0 \
+#			 -lgobject-2.0 -lbz2 -lpangocairo-1.0 -llcms2 -llqr-1 -lfftw3 -lltdl \
+#			 /work/libfuzzer/Fuzzer*.o  -o ImageMagick_fuzzer ImageMagick_fuzzer.c
--- dockerfiles/libav/libav_fuzzer.cc
+++ dockerfiles/libav/libav_fuzzer.cc
@@ -24,7 +24,5 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   codec_context = NULL;
 
 
-
-
   return 0;
 }
