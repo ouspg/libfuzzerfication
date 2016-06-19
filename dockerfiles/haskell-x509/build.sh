e75b6845ab33be3ced82d3b7d097e6315e6b1c44
Ossi Herrala <oherrala@gmail.com>
2016-06-19 22:01:43 +0300

    haskell-x509: Add custom instrumentation
    Exceptions from Data.X509.decodeSignedCertificate are errors. Catch
    the exceptions and store test case to disk for future analysis.
    
    Now test.hs should runs forever (or until it rans out of disk space).

--- dockerfiles/haskell-x509/build.sh
+++ dockerfiles/haskell-x509/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GHCOPTS="-package bytestring -package x509"
+GHCOPTS="-Wall -package bytestring -package cryptohash -package directory -package x509"
 
 ghc-asan ${GHCOPTS} -c test.hs
 ghc-wrapper ${GHCOPTS} -no-hs-main -o test test.o /work/libfuzzer/*.o
--- dockerfiles/haskell-x509/test.hs
+++ dockerfiles/haskell-x509/test.hs
@@ -1,20 +1,62 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Strict #-}
 
 module Test where
 
 import Foreign.C.Types
 import Foreign.C.String
-
 import qualified Data.ByteString as BS
 
+import Control.Monad
+import Control.Exception
 import qualified Data.X509 as X509
+
+import Data.Byteable (toBytes)
+import System.IO
+import System.Directory (createDirectoryIfMissing, doesFileExist)
+import Crypto.Hash.SHA1 (hash)
+import qualified Data.ByteString.Char8 as Char8
+import qualified Data.ByteArray.Encoding as BA
 
 -- extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size)
 foreign export ccall "LLVMFuzzerTestOneInput" testOneInputM :: CString -> CSize -> IO CInt
 
+hashInHex :: String -> String
+hashInHex = hashInHexBS . Char8.pack
+
+hashInHexBS :: BS.ByteString -> String
+hashInHexBS =  Char8.unpack . BA.convertToBase BA.Base16 . toBytes . hash
+
+storeTestCase :: String -> BS.ByteString -> IO ()
+storeTestCase name content =
+  -- Use first 60 bytes for generating directory name.  This adds
+  -- different variants of almost same exception into same directory
+  let dir = "results/" ++ hashInHex (take 60 name)
+      file = hashInHexBS content
+  in do
+    createDirectoryIfMissing True dir
+    hasReadme <- doesFileExist $ dir ++ "/00README"
+    unless hasReadme (
+      withFile (dir ++ "/00README") WriteMode $ \h ->
+          BS.hPut h $ Char8.pack (name ++ "\n")
+      )
+    withFile (dir ++ "/" ++ file) WriteMode $ \h ->
+      BS.hPut h content
+
+decode :: BS.ByteString -> IO (Either SomeException (Either String X509.SignedCertificate))
+decode = try . evaluate . X509.decodeSignedCertificate
+
 testOneInputM :: CString -> CSize -> IO CInt
 testOneInputM str size = do
   bs <- BS.packCStringLen (str, fromIntegral size)
-  let result = X509.decodeSignedCertificate bs
+
+  result <- decode bs
+  case result of
+    Right _  -> return ()
+    Left err -> do
+      let err' = show err
+      putStrLn $ "Exception: " ++ err'
+      storeTestCase err' bs
+
   return 0 -- Non-zero return values are reserved for future use.
