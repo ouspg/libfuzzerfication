0014325b1068419ca279346d585ac0941183354c
Atte Kettunen <attekett@gmail.com>
2016-06-20 16:58:29 +0300

    Looks like libav_fuzzer is now working. Need to verify that data is passed correctly.

--- dockerfiles/libav/libav_fuzzer.c
+++ dockerfiles/libav/libav_fuzzer.c
@@ -6,6 +6,7 @@
 extern "C" {
 #include "libavcodec/avcodec.h"
 #include "libavutil/avutil.h"
+#include "libavutil/mathematics.h"
 }
 
 #define INBUF_SIZE 4096
@@ -13,68 +14,59 @@ extern "C" {
 #define AUDIO_REFILL_THRESH 4096
 
 
+//TODO: Remove extra check etc.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+	avcodec_register_all();
+	av_log_set_level(-1);
+	AVCodec *codec;
+	AVCodecContext *c= NULL;
+	int len;
+	AVPacket avpkt;
+	AVFrame *decoded_frame = NULL;
 
-  /* register all the codecs */
-  avcodec_register_all();
+	av_init_packet(&avpkt);
 
-  AVCodec *codec;
-  AVCodecContext *c= NULL;
-  int len;
-  FILE *f, *outfile;
-  uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
-  AVPacket avpkt;
-  AVFrame *decoded_frame = NULL;
+	codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
+	  if (!codec) {
+	      fprintf(stderr, "codec not found\n");
+	      exit(1);
+	  }
 
-  av_init_packet(&avpkt);
+	c = avcodec_alloc_context3(codec);
 
-  codec = avcodec_find_decoder(AV_CODEC_ID_MP2);
-      if (!codec) {
-          fprintf(stderr, "codec not found\n");
-          exit(1);
-      }
+	if (avcodec_open2(c, codec, NULL) < 0) {
+	       fprintf(stderr, "could not open codec\n");
+	       exit(1);
+	   }
 
-  c = avcodec_alloc_context3(codec);
+	/* decode until eof */
+	//TODO: Check that the data is actually read as it should.
+	avpkt.data = (uint8_t *) data;
+	avpkt.size = size; //fread(inbuf, 1, AUDIO_INBUF_SIZE, f);
 
-  if (avcodec_open2(c, codec, NULL) < 0) {
-           fprintf(stderr, "could not open codec\n");
-           exit(1);
-       }
+	while (avpkt.size > 0) {
+		int got_frame = 0;
 
-  /* decode until eof */
-  avpkt.data = inbuf;
-  avpkt.size = fread(inbuf, 1, AUDIO_INBUF_SIZE, f);
+		if (!decoded_frame) {
+			if (!(decoded_frame = av_frame_alloc())) {
+				fprintf(stderr, "out of memory\n");
+				exit(1);
+			}
+		}
 
-  while (avpkt.size > 0) {
-  int got_frame = 0;
+		len = avcodec_decode_audio4(c, decoded_frame, &got_frame, &avpkt);
+		if (len < 0) {
+			//fprintf(stderr, "Error while decoding\n");
+			break;
+		}
 
-  if (!decoded_frame) {
-    if (!(decoded_frame = av_frame_alloc())) {
-      fprintf(stderr, "out of memory\n");
-      exit(1);
-      }
-  }
+		avpkt.size -= len;
+		avpkt.data += len;
 
-  len = avcodec_decode_audio4(c, decoded_frame, &got_frame, &avpkt);
-  if (len < 0) {
-    fprintf(stderr, "Error while decoding\n");
-    exit(1);
-  }
+	}
 
-  avpkt.size -= len;
-  avpkt.data += len;
-  if (avpkt.size < AUDIO_REFILL_THRESH) {
-      memmove(inbuf, avpkt.data, avpkt.size);
-      avpkt.data = inbuf;
-      len = fread(avpkt.data + avpkt.size, 1,
-                           AUDIO_INBUF_SIZE - avpkt.size, f);
-      if (len > 0)
-          avpkt.size += len;
-      }
-    }
+	avcodec_close(c);
+	av_free(c);
 
-  avcodec_close(c);
-  av_free(c);
-
-  return 0;
+	return 0;
 }
