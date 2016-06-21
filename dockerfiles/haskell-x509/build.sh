75f1db2c63ee23ac7e86ea8ee1bd8cb38c90dd28
Pauli Huttunen <pauli.huttunen@outlook.com>
2016-06-21 16:46:09 +0300

    new directory layout

--- dockerfiles/haskell-x509/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-GHCOPTS="-Wall -package bytestring -package cryptohash -package directory -package x509"
-
-ghc-asan ${GHCOPTS} -c test.hs
-ghc-wrapper ${GHCOPTS} -no-hs-main -o test test.o /work/libfuzzer/*.o
--- dockerfiles/libav.sh
+++
@@ -1,5 +0,0 @@
-set -e -x
-
-docker build $* -t libfuzzer/libfuzzer-base libfuzzer-base/
-
-docker build $* -t libfuzzer/libav libav/
