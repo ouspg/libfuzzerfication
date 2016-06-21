92079eab4dc17fcc40bf3fce6d66bab9b0e94289
Ossi Herrala <oherrala@gmail.com>
2016-06-21 16:47:02 +0300

    Use Libfuzzer Haskell lib

--- dockerfiles/haskell-x509/build.sh
+++ dockerfiles/haskell-x509/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-GHCOPTS="-Wall -package bytestring -package cryptohash -package directory -package x509"
+set -ex
 
+GHCOPTS="-Wall -package libfuzzer -package x509"
+
+clang -Wall -c -I/opt/ghc/8.0.1/lib/ghc-8.0.1/include/ hsinit.c
 ghc-asan ${GHCOPTS} -c test.hs
-ghc-wrapper ${GHCOPTS} -no-hs-main -o test test.o /work/libfuzzer/*.o
+ghc-wrapper ${GHCOPTS} -no-hs-main -lFuzzer -o test test.o hsinit.o
