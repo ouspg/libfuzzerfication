307bc75c2c73122ac8ec9a2836519af1af8839f0
Mikko Yliniemi <mikessu@iki.fi>
2016-06-23 15:23:09 +0300

    minor modifications

--- docker/stubs/flac/flac_fuzzer.c
+++ docker/stubs/flac/flac_fuzzer.c
@@ -5,12 +5,12 @@
 
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	FLAC__bool ok = true;
-	FLAC__StreamDecoder *decoder = 0;
+	/*FLAC__bool ok = true;
+	#FLAC__StreamDecoder *decoder = 0;
 	FLAC__StreamDecoderInitStatus init_status;
 
 	(void)FLAC__stream_decoder_set_md5_checking(decoder, true);
-
+*/
 	// todo: todo
 
 	return 0;
