3f790e31db5195864f824a03cb597e38f90059c6
Mikko Yliniemi <mikessu@iki.fi>
2016-07-01 10:48:22 +0300

    Improved flac dockerfile and build.sh code

--- docker/stubs/flac/build.sh
+++ docker/stubs/flac/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
-export CXXFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
 export CXX="clang++"
-export CFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
 export CC="clang"
-export LDFLAGS="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
+
+export SANCOV="-fsanitize=address -fsanitize-coverage=edge,indirect-calls"
+
+export CXXFLAGS=$SANCOV
+export CFLAGS=$SANCOV
+export LDFLAGS=$SANCOV
+
 
 #Build flac
-cd /src/flac
+#cd /src/flac
 if [ ! -f configure ]
 then
   ./autogen.sh
@@ -17,4 +20,4 @@ make -j 4
 
 $CXX $CXXFLAGS -std=c++11 flac_fuzzer.c \
   			   -Iinclude -L.libs -lFuzzer \
-   			    -o /src/flac/flac_fuzzer
+   			    -o flac_fuzzer
