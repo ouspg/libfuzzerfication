5013f9b91fec96f691b28a514622a0661ca521ce
Mikko Yliniemi <mikessu@iki.fi>
2016-07-07 12:53:27 +0300

    Flac fuzzer updated

--- docker/stubs/flac/flac_fuzzer.c
+++ docker/stubs/flac/flac_fuzzer.c
@@ -3,9 +3,34 @@
 #include "share/compat.h"
 #include "FLAC/stream_decoder.h"
 
-static void error_callback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *data);
-static FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 * const buffer[], void *data);
-static FLAC__StreamDecoderReadStatus read_callback(const FLAC__StreamDecoder *decoder, FLAC__byte buffer[], size_t *bytes, void *data);
+static void error_callback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *client_data);
+static FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 * const buffer[], void *client_data);
+
+/*
+"A function pointer matching this signature must be passed to FLAC__stream_decoder_init*_stream().
+The supplied function will be called when the decoder needs more input data. The address of the buffer
+to be filled is supplied, along with the number of bytes the buffer can hold. The callback may choose
+to supply less data and modify the byte count but must be careful not to overflow the buffer.
+The callback then returns a status code chosen from FLAC__StreamDecoderReadStatus."
+ref https://xiph.org/flac/api/group__flac__stream__decoder.html
+*/
+
+static FLAC__StreamDecoderReadStatus read_callback( const FLAC__StreamDecoder *decoder, FLAC__byte buffer[], size_t *bytes, void *client_data)
+{
+  //FLAC_Data *data = (FLAC_Data *)client_data;
+  //if(*bytes > 0) {
+    //*bytes = fread(buffer, sizeof(FLAC__byte), *bytes, file);
+    /*if(ferror(file))
+      return FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+    else if(*bytes == 0)
+      return FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
+    else
+      return FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
+    }
+    else
+      return FLAC__STREAM_DECODER_READ_STATUS_ABORT;*/
+}
+
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 	FLAC__bool ok = true;
@@ -20,16 +45,16 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
 	(void)FLAC__stream_decoder_set_md5_checking(decoder, true);
 
-	init_status = FLAC__stream_decoder_init_stream ( 	decoder,
-																										read_callback,
-																										/*seek_callback*/ NULL,
-																										/*tell_callback*/ NULL,
-																										/*length_callback*/ NULL,
-																										/*eof_callback*/ NULL,
-																										write_callback,
-																										/*metadata_callback*/ NULL,
-																										error_callback,
-																										data);
+	//init_status = FLAC__stream_decoder_init_stream ( 	decoder,
+	//																									read_callback,
+	//																									/*seek_callback*/ NULL,
+	//																									/*tell_callback*/ NULL,
+	//																									/*length_callback*/ NULL,
+	//																									/*eof_callback*/ NULL,
+	//																									write_callback,
+	//																									/*metadata_callback*/ NULL,
+	//																									error_callback,
+	//																									client_data);
 
 	if(init_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
 		fprintf(stderr, "ERROR: initializing decoder: %s\n", FLAC__StreamDecoderInitStatusString[init_status]);
@@ -47,8 +72,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 	return 0;
 }
 
-void error_callback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *data)
+void error_callback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *client_data)
 {
-	(void)decoder, (void)data;
+	(void)decoder, (void)client_data;
   fprintf(stderr, "Got error callback: %s\n", FLAC__StreamDecoderErrorStatusString[status]);
 }
