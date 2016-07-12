03711a1cb14be3d916db4d3f28f306311d3615cf
Marko Laakso <fenris@iki.fi>
2016-07-12 17:10:01 +0300

    mysamplelib stub build

--- docker/stubs/mysamplelib/build.sh
+++ docker/stubs/mysamplelib/build.sh
@@ -1,3 +1,3 @@
 clang -fsanitize=address \
       -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp \
-      mysamplelib.c fuzzer.c -lFuzzer -o fuzzer
+      mysamplelib.c fuzzer.c -L/usr/local/lib -lFuzzer -o fuzzer
