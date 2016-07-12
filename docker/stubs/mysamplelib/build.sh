f89a0a6a3cc4a1bb84a7c5b685934f6f0d674181
Marko Laakso <fenris@iki.fi>
2016-07-12 22:34:51 +0300

    mysamplelib: cleaner build still

--- docker/stubs/mysamplelib/build.sh
+++ docker/stubs/mysamplelib/build.sh
@@ -1,3 +1,13 @@
-clang++ -fsanitize=address \
-        -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp \
-        mysamplelib.c fuzzer.c -lFuzzer -o fuzzer
+#!/bin/sh
+
+CFLAGS="-g -fsanitize=address \
+        -fsanitize-coverage=edge,indirect-calls,8bit-counters,trace-cmp"
+
+## compile the library
+clang   ${CFLAGS} -c mysamplelib.c
+
+## compile the fuzzing stubs
+clang   ${CFLAGS} -c fuzzer.c
+
+## link them together
+clang++ ${CFLAGS} mysamplelib.o fuzzer.o -lFuzzer -o fuzzer
