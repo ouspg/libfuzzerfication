19432b4d34f112560e418aa502ae23b2846bd448
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-08-18 13:13:17 +0300

    updated rust fuzzer

--- stubs/rust/rust-fuzzer.cc
+++ stubs/rust/rust-fuzzer.cc
@@ -1,16 +1,8 @@
-/*
- * lua-fuzzer.c
- * libfuzzer stub for fuzzing LUA
- */
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-extern "C" {
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-}
+
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
