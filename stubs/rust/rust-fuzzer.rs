d27c5fb83ba8291ca09401c1db2aed2b2e98acf6
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-08-25 15:32:41 +0300

    updated rust fuzzer

--- stubs/rust/rust-fuzzer.rs
+++ stubs/rust/rust-fuzzer.rs
@@ -1,16 +1,18 @@
+extern crate libc;
+
 use std::io::{self, Read};
 
 #[no_mangle]
-pub extern fn LLVMFuzzerTestOneInput(std::ffi::CString) -> *const u8 {
+pub extern fn LLVMFuzzerTestOneInput(std::ffi::CString *data, libc::size_t size) -> *const u8 {
 let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    if input.starts_with("x") {
-        println!("going...");
-        if input.starts_with("xy") {
-            println!("going...");
-            if input.starts_with("xyz") {
-                println!("gone!");
+    if input.starts_with("a") {
+        println!("a");
+        if input.starts_with("ab") {
+            println!("ab");
+            if input.starts_with("abc") {
+                println!("abc");
                 unsafe {
                     let x: *mut usize = 0 as *mut usize;
                     *x = 0xBEEF;
@@ -18,4 +20,4 @@ let mut input = String::new();
             }
         }
     }
-} 
+}
