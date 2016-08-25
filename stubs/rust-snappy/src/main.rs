35efa48248f2010452e27f43c68834a0ad5138d8
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-08-25 14:39:35 +0300

    added rust-snappy, wip

---
+++ stubs/rust-snappy/src/main.rs
@@ -0,0 +1,12 @@
+extern crate libc;
+use libc::size_t;
+
+#[link(name = "snappy")]
+extern {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+}
+
+fn main() {
+    let x = unsafe { snappy_max_compressed_length(100) };
+    println!("max compressed length of a 100 byte buffer: {}", x);
+}
