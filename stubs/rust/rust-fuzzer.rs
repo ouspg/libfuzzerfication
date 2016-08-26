bf175edb126d2154b4cc349853180d032006ea77
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2016-08-26 14:48:43 +0300

    Added very simple rust stub

--- stubs/rust/rust-fuzzer.rs
+++ stubs/rust/rust-fuzzer.rs
@@ -1,25 +1,28 @@
+#![feature(libc)]
+#[crate_type = "lib"]
+#[no_std]
+#[allow(ctypes)]
+
 extern crate libc;
 
 use std::io::{self, Read};
 
 #[no_mangle]
-pub extern fn LLVMFuzzerTestOneInput(std::ffi::CString *data, libc::size_t size) -> *const u8 {
-println!("hello");
+pub extern fn LLVMFuzzerTestOneInput(data: *const std::ffi::CString, size: libc::size_t) {
+let mut input = String::new();
+    println!("starting!");
+    if input.starts_with("a") {
+        println!("a");
+        if input.starts_with("ab") {
+            println!("ab");
+            if input.starts_with("abc") {
+                println!("abc");
+                unsafe {
+                    let x: *mut usize = 0 as *mut usize;
+                    *x = 0xBEEF;
+                }
+            }
+        }
+    }
 
-///let mut input = String::new();
-///    io::stdin().read_to_string(&mut input).unwrap();
-///
-///    if input.starts_with("a") {
-///        println!("a");
-///        if input.starts_with("ab") {
-///            println!("ab");
-///            if input.starts_with("abc") {
-///                println!("abc");
-///                unsafe {
-///                    let x: *mut usize = 0 as *mut usize;
-///                    *x = 0xBEEF;
-///                }
-///            }
-///        }
-///    }
 }
